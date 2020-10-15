44ece79c1faebbd0a11a055f409b0dd62b60376d
dicky perdian <dicky.perdian@digitalline.id>
2020-10-15 16:50:56 +0700

    update cost

--- src/RajaOngkir.php
+++ src/RajaOngkir.php
@@ -1,5 +1,6 @@
 <?php
 namespace Dickyp\RajaOngkir;
+use Exception;
 
 class RajaOngkir{
 	protected $endpoint;
@@ -22,27 +23,23 @@ class RajaOngkir{
 			CURLOPT_MAXREDIRS 		=> 10,
 			CURLOPT_TIMEOUT 		=> 30,
 			CURLOPT_HTTP_VERSION 	=> CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST 	=> "GET",
-			CURLOPT_HTTPHEADER 		=> array(
-		    	"key:".$this->key
-			)
 		);
 
-		// if ($options) {
- 		//	$config = array_merge($default, $options);
-		// } else{
-		// 	$data = [
-		// 		CURLOPT_URL 			=> $url,
-		// 		CURLOPT_CUSTOMREQUEST 	=> "GET",
-		// 		CURLOPT_HTTPHEADER 		=> array(
-		// 	    	"key:".$this->key
-		// 		)
-		// 	];
-		// 	$config = array_merge($default, $data);
-		// }
+		if ($options) {
+ 			$config = $default+$options;
+		} else{
+			$data = [
+				CURLOPT_URL 			=> $url,
+				CURLOPT_CUSTOMREQUEST 	=> "GET",
+				CURLOPT_HTTPHEADER 		=> array(
+			    	"key:".$this->key
+				)
+			];
+			$config = $default+$data;
+		}
 		
 
-		curl_setopt_array($curl, $default);
+		curl_setopt_array($curl, $config);
 
 		$response = curl_exec($curl);
 		$err 	  = curl_error($curl);
@@ -55,7 +52,6 @@ class RajaOngkir{
 		
 		if (!isset($response_convert['rajaongkir'])) {
 			$this->error = 'Response not valid';
-			//return false;
 		}
 		$rajaongkir = $response_convert['rajaongkir'];
 
@@ -67,14 +63,6 @@ class RajaOngkir{
 			$data 		 = $rajaongkir['results'];
 			return $data;
 		}
-		// if ($err) {
-		// 	echo "cURL Error #:" . $err;
-		// } else {
-		// 	$response = json_decode($response,true);
-		// 	$data 	  = $response['rajaongkir']['results'];
-		// 	return $data;
-		// }
-		
 	}
 
 	public function get_city($id = null){
@@ -107,66 +95,14 @@ class RajaOngkir{
 
 
 	public function cost_shipping($origin, $destination, $weight, $courier){
-		$curl = curl_init();
-
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL 			 => $this->endpoint."/cost",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING 		 => "",
-		  CURLOPT_MAXREDIRS 	 => 10,
-		  CURLOPT_TIMEOUT 		 => 30,
-		  CURLOPT_HTTP_VERSION 	 => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST  => "POST",
-		  CURLOPT_POSTFIELDS 	 => "origin=".$origin."&destination=".$destination."&weight=".$weight."&courier=".$courier,
-		  CURLOPT_HTTPHEADER 	 => array(
-		    "content-type: application/x-www-form-urlencoded",
-		    "key:".$this->key
-		  ),
-		));
-
-		$response = curl_exec($curl);
-		$err 	  = curl_error($curl);
-		$response_convert = json_decode($response,true);
-		
-		curl_close($curl);
-		if ($err) {
-			throw new Exception($err, 1);
-		}
-		
-		if (!isset($response_convert['rajaongkir'])) {
-			$this->error = 'Response not valid';
-			return false;
-		}
-
-		$rajaongkir = $response_convert['rajaongkir'];
-
-		if ($response_convert['rajaongkir']['status']['code'] == 400 ) {
-			$this->error = $rajaongkir['status']['description'];
-		}
-
-		if ($response_convert['rajaongkir']['status']['code'] == 200 ) {
-			$data 		 = $rajaongkir['results'];
-			return $data;
-		}
-
-		// if ($err) {
-		// 	echo "cURL Error #:" . $err;
-		// } else {
-		// 	$response = json_decode($response,true);
-		// 	$data 	  = $response['rajaongkir']['results'];
-		// 	return $data;
-		// 	dd($response);
-		// }
-
-		// $new_addon_options = [
-		// 	CURLOPT_URL 			=> $this->endpoint."/cost",
-		// 	CURLOPT_CUSTOMREQUEST   => "POST",
-		// 	CURLOPT_POSTFIELDS 		=> "origin=".$origin."&destination=".$destination."&weight=".$weight."&courier=".$courier,
-		// 	CURLOPT_HTTPHEADER 		=> array(
-		// 		"content-type: application/x-www-form-urlencoded",
-		//     	"key:".$this->key
-		// 	),
-		// ];
-		// return self::_request('/cost', $new_addon_options);
+		$new_addon_options = [
+			CURLOPT_CUSTOMREQUEST   => "POST",
+			CURLOPT_POSTFIELDS 		=> "origin=".$origin."&destination=".$destination."&weight=".$weight."&courier=".$courier,
+			CURLOPT_HTTPHEADER 		=> array(
+				"content-type: application/x-www-form-urlencoded",
+		    	"key:".$this->key
+			),
+		];
+		return self::_request('/cost', $new_addon_options);
 	}
 }
